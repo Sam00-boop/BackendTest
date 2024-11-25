d35e13982f5f7d4c21d83a0e7ea00be439c31d36
Samuele Sicura <ICTS23-25.084@studenti.local>
2024-11-25 18:00:01 +0100

    aggiunto controller

---
+++ GestioneOrdini/GestioneOrdini/OrdersController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace GestioneOrdini
+{
+    internal class OrdersController
+    {
+        public List<Order> GetOrders(string path) 
+        {
+            List<Order> orders = new List<Order>();
+
+            try
+            {
+                // Controlla se il file esiste
+                if (!File.Exists(path))
+                {
+                    throw new FileNotFoundException("Il file specificato non è stato trovato.");
+                }
+
+                // Legge il file riga per riga
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string riga = sr.ReadLine();
+                    while ((riga = sr.ReadLine()) != null)
+                    {
+                        // Separa la riga corrente in un array
+                        string[] dati = riga.Split(",");
+
+                        // Creazione e inizializzazione dell'oggetto order 
+                        var ordine = new Order
+                        {
+                            Id = long.Parse(dati[0]),
+                            Article = dati[1],
+                            Quantity = long.Parse(dati[2]),
+                            Price = double.Parse(dati[3]),
+                            Discount = double.Parse(dati[4]),
+                            Buyer = dati[5]
+                        };
+
+                        orders.Add(ordine);
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"Errore: {ex.Message}");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine($"Errore: Accesso al file negato. {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Errore: {ex.Message}");
+            }
+
+            return orders;
+        }
+
+
+        public Order MaxPrice(List<Order> elenco)
+        {
+            return elenco.OrderByDescending(a => a.TotalPrice()).First();
+        }
+
+        public Order MaxQuantity(List<Order> elenco)
+        {
+            return elenco.OrderByDescending(a => a.Quantity).First();
+        }
+
+        public Order MaxPriceDiff(List<Order> elenco)
+        {
+            return elenco.OrderByDescending(a => a.PriceDifference()).First();
+        }
+    }
+}
--- GestioneOrdini/GestioneOrdini/Program.cs
+++ GestioneOrdini/GestioneOrdini/Program.cs
@@ -1,4 +1,4 @@
-﻿using BackendTest;
+﻿using GestioneOrdini;
 
 namespace GestioneOrdini
 {
@@ -6,69 +6,82 @@ namespace GestioneOrdini
     {
         static void Main(string[] args)
         {
-            // Percorso del file CSV
-            Console.Write("Inserire il percorso del file csv da cui leggere:");
-            string input = Console.ReadLine();
-            //string path = @"..\..\..\files\file.csv";
+            
+            List<Order> elenco = new List<Order>(); // Elenco di ordini
+            bool fileLetto = false;
+            OrdersController ctrl = new OrdersController();
+            string input;
 
-            // Elenco di articoli
-            List<Order> elenco = new List<Order>();
-
-
-            try
+            while (!fileLetto) // Ciclo per chiedere all'utente il percorso finchè non ne da uno valido
             {
-                // Lettura del file CSV
-                using (StreamReader sr = new StreamReader(input))
+                Console.Write("Inserire il percorso del file csv da cui leggere:");
+                input = Console.ReadLine(); // Percorso del file CSV
+                elenco = ctrl.GetOrders(input);
+                if (elenco.Count > 0)
+                    fileLetto = true;
+                foreach (Order o in elenco) 
+                    Console.WriteLine(o);
+            }
+
+           
+
+            /*
+                try
                 {
-                    string riga = sr.ReadLine(); // Legge l'intestazione (prima riga)
-
-                    while ((riga = sr.ReadLine()) != null)
+                    // Lettura del file CSV
+                    using (StreamReader sr = new StreamReader(input))
                     {
-                        // Separa la riga corrente in un array
-                        string[] dati = riga.Split(",");
+                        string riga = sr.ReadLine(); // Legge l'intestazione (prima riga)
 
-                        // Creazione e inizializzazione dell'oggetto order 
-                        var ordine = new Order
+                        while ((riga = sr.ReadLine()) != null)
                         {
-                            Id = long.Parse(dati[0]),
-                            Article = dati[1],
-                            Quantity = long.Parse(dati[2]),
-                            Price = double.Parse(dati[3]),
-                            Discount = double.Parse(dati[4]),
-                            Buyer = dati[5]
-                        };
+                            // Separa la riga corrente in un array
+                            string[] dati = riga.Split(",");
 
-                        // Aggiunta dell'oggetto all'elenco
-                        elenco.Add(ordine);
+                            // Creazione e inizializzazione dell'oggetto order 
+                            var ordine = new Order
+                            {
+                                Id = long.Parse(dati[0]),
+                                Article = dati[1],
+                                Quantity = long.Parse(dati[2]),
+                                Price = double.Parse(dati[3]),
+                                Discount = double.Parse(dati[4]),
+                                Buyer = dati[5]
+                            };
 
-                        Console.WriteLine(ordine);
+                            // Aggiunta dell'oggetto all'elenco
+                            elenco.Add(ordine);
 
+                            Console.WriteLine(ordine);
+
+                        }
+                    }
+
+                    OrdersController ctrl = new OrdersController();
+
+                    // Record con importo totale più alto (per importo totale sto considerando prezzoScontato*quantita)
+                    Console.WriteLine($"\nRecord con importo totale più alto: {ctrl.MaxPrice(elenco)}");
+
+                    // Record con quantità più alta
+                    Console.WriteLine($"\nRecord con quantità più alta: {ctrl.MaxQuantity(elenco)}");
+
+                    // Record con maggior differenza tra totale senza sconto e totale con sconto
+                    Console.WriteLine($"\nRecord con maggior differenza tra totale senza sconto e totale con sconto: {ctrl.MaxPriceDiff(elenco)}");
+
+                    fileLetto = true;
+
+                    foreach (var article in elenco)
+                    {
+                        //Console.WriteLine(article.Price);
+                        //Console.WriteLine(article.DiscountPrice());
+                        //Console.WriteLine(article.TotalPrice());
                     }
                 }
-
-                // Record con importo totale più alto (per importo totale sto considerando prezzoScontato*quantita)
-                var maxTotalArticle = elenco.OrderByDescending(a => a.TotalPrice()).First();
-                Console.WriteLine($"Record con importo totale più alto: {maxTotalArticle}");
-
-                // Record con quantità più alta
-                maxTotalArticle = elenco.OrderByDescending(a => a.Quantity).First();
-                Console.WriteLine($"Record con quantità più alta: {maxTotalArticle}");
-
-                // Record con maggior differenza tra totale senza sconto e totale con sconto
-                maxTotalArticle = elenco.OrderByDescending(a => a.PriceDifference()).First();
-                Console.WriteLine($"Record con maggior differenza tra totale senza sconto e totale con sconto: {maxTotalArticle}");
-
-                foreach (var article in elenco)
+                catch (Exception ex)
                 {
-                    //Console.WriteLine(article.Price);
-                    //Console.WriteLine(article.DiscountPrice());
-                    //Console.WriteLine(article.TotalPrice());
+                    Console.WriteLine($"Errore durante la lettura del file: {ex.Message}");
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Errore durante la lettura del file: {ex.Message}");
-            }
+            */
         }
     }
 }
