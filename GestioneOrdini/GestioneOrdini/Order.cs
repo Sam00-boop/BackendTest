bdaf462fc144204c085af7ad3aec47fd7e343c28
Samuele Sicura <ICTS23-25.084@studenti.local>
2024-11-25 15:42:29 +0100

    aggiunto GestioneOrdini

---
+++ GestioneOrdini/GestioneOrdini/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendTest
+{
+    internal class Order
+    {
+
+        public long Id { get; set; }
+        public string Article {  get; set; }
+        public long Quantity { get; set; }
+        public double Price { get; set; }
+        public double Discount { get; set; }
+        public string Buyer { get; set; }
+
+        public double DiscountPrice()
+        {
+            return Price-Price*Discount/100; 
+        }
+
+        public double TotalPrice()
+        {
+            return Quantity * DiscountPrice();
+        }
+
+        public double PriceDifference()
+        {
+            return Price - DiscountPrice();
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Id)}={Id.ToString()}, {nameof(Article)}={Article}, {nameof(Quantity)}={Quantity.ToString()}, {nameof(Price)}={Price.ToString()}, {nameof(Discount)}={Discount.ToString()}, {nameof(Buyer)}={Buyer}}}";
+        }
+    }
+}
---
+++ GestioneOrdini/GestioneOrdini/Program.cs
@@ -0,0 +1,74 @@
+﻿using BackendTest;
+
+namespace GestioneOrdini
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Percorso del file CSV
+            Console.Write("Inserire il percorso del file csv da cui leggere:");
+            string input = Console.ReadLine();
+            //string path = @"..\..\..\files\file.csv";
+
+            // Elenco di articoli
+            List<Order> elenco = new List<Order>();
+
+
+            try
+            {
+                // Lettura del file CSV
+                using (StreamReader sr = new StreamReader(input))
+                {
+                    string riga = sr.ReadLine(); // Legge l'intestazione (prima riga)
+
+                    while ((riga = sr.ReadLine()) != null)
+                    {
+                        // Separa la riga corrente in un array
+                        string[] dati = riga.Split(",");
+
+                        // Creazione e inizializzazione dell'oggetto order 
+                        var ordine = new Order
+                        {
+                            Id = long.Parse(dati[0]),
+                            Article = dati[1],
+                            Quantity = long.Parse(dati[2]),
+                            Price = double.Parse(dati[3]),
+                            Discount = double.Parse(dati[4]),
+                            Buyer = dati[5]
+                        };
+
+                        // Aggiunta dell'oggetto all'elenco
+                        elenco.Add(ordine);
+
+                        Console.WriteLine(ordine);
+
+                    }
+                }
+
+                // Record con importo totale più alto (per importo totale sto considerando prezzoScontato*quantita)
+                var maxTotalArticle = elenco.OrderByDescending(a => a.TotalPrice()).First();
+                Console.WriteLine($"Record con importo totale più alto: {maxTotalArticle}");
+
+                // Record con quantità più alta
+                maxTotalArticle = elenco.OrderByDescending(a => a.Quantity).First();
+                Console.WriteLine($"Record con quantità più alta: {maxTotalArticle}");
+
+                // Record con maggior differenza tra totale senza sconto e totale con sconto
+                maxTotalArticle = elenco.OrderByDescending(a => a.PriceDifference()).First();
+                Console.WriteLine($"Record con maggior differenza tra totale senza sconto e totale con sconto: {maxTotalArticle}");
+
+                foreach (var article in elenco)
+                {
+                    //Console.WriteLine(article.Price);
+                    //Console.WriteLine(article.DiscountPrice());
+                    //Console.WriteLine(article.TotalPrice());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Errore durante la lettura del file: {ex.Message}");
+            }
+        }
+    }
+}
